05f688fc39eacf3e90dd1aad0f35b38952824ada
tamtam2008 <37857703+tamtam2008@users.noreply.github.com>
2019-07-13 17:30:14 +0700

    first commit

---
+++ ProjectNghiPhep/Controllers/DonNghiPhepController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectNghiPhep.Controllers
+{
+    public class DonNghiPhepController : Controller
+    {
+        //
+        // GET: /DonNghiPhep/
+           
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
