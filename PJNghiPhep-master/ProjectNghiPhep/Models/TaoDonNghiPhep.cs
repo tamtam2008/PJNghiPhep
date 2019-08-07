27531de114293faf2418734d72663adba26f4bec
Ho Phuoc Sieu <sieusjnc698@gmail.com>
2019-08-07 19:18:41 +0700

    add dangki

---
+++ PJNghiPhep-master/ProjectNghiPhep/Models/TaoDonNghiPhep.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace ProjectNghiPhep.Models
+{
+    public class TaoDonNghiPhep
+    {
+        [Required(ErrorMessage = "Trường này là bắt buộc")]
+        [Display(Name = "Ngày bắt đầu")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Invalid")]
+        public string dateStart { get; set; }
+        [Required(ErrorMessage = "Trường này là bắt buộc")]
+        [Display(Name = "Ngày kết thúc")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Invalid")]
+        public string dateEnd { get; set; }
+        [Required(ErrorMessage = "Trường này là bắt buộc")]
+        [Display(Name = "Lý do")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Invalid")]
+        public string reason { get; set; }
+    }
+}
